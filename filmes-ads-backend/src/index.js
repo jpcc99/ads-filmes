2b41839faea15b3d1e2c75ecd722a0935d2b962e
jpcc99 <jpcc.dev@gmail.com>
2025-07-24 16:15:32 -0300

    Hello world pelo servidor

--- filmes-ads-backend/src/index.js
+++ filmes-ads-backend/src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+const server = require("./server");
+const port = 3001;
+
+server.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+})
+
+server.listen(port, () => console.log(`Servidor ouvindo na porta ${port}`));
