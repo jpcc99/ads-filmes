7db212d6f56c486a843e3582a8646823050ac2b5
jpcc99 <jpcc.dev@gmail.com>
2025-07-24 16:33:18 -0300

    Add routes, fix onde morgan deve estar

--- filmes-ads-backend/src/index.js
+++ filmes-ads-backend/src/index.js
@@ -1,11 +1,4 @@
 const server = require("./server");
-const morgan = require("morgan");
 const port = 3001;
-
-server.use(morgan("dev"));
-
-server.get("/", (req, res) => {
-  res.status(200).send("Hello World");
-})
 
 server.listen(port, () => console.log(`Servidor ouvindo na porta ${port}`));
--- filmes-ads-backend/src/server.js
+++ filmes-ads-backend/src/server.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const server = express();
+const routes = require("./routes/");
+
+server.use(morgan("dev"));
+
+server.use("/", routes);
 
 module.exports = server;
