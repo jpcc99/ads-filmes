25b60627f4eadae1d2a38a4b395113fe53923e46
jpcc99 <jpcc.dev@gmail.com>
2025-07-24 17:00:12 -0300

    Adds um get movie por id

--- filmes-ads-backend/src/controllers/MoviesController.js
+++ filmes-ads-backend/src/controllers/MoviesController.js
@@ -3,7 +3,19 @@
  * @param{import('express').Request} req
  * @param{import('express').Response} res
 */
-module.exports.listar = function(req, res) {
+module.exports.listar = function(_req, res) {
   const movies = ["lista", "de", "filmes"];
   return res.status(200).json(movies);
+}
+
+/**
+ * Getter de detalhes de um dado filme. 
+ * @param{import('express').Request} req
+ * @param{import('express').Response} res
+*/
+module.exports.getDetailsById = function(req, res) {
+  const { id } = req.params;
+
+  console.log("movie_id: " + id);
+  return res.status(404).send("Busca ainda não disponível");
 }
--- filmes-ads-backend/src/routes/movies.js
+++ filmes-ads-backend/src/routes/movies.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const MoviesController = require("../controllers/MoviesController");
 
 router.get("/", MoviesController.listar);
+router.get("/:id", MoviesController.getDetailsById);
 
 module.exports = router;
